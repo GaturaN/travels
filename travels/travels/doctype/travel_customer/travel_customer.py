624733ecc73dea1089239c193d6c1b48f1780115
GaturaN <gaturanjenga@gmail.com>
2024-07-29 20:29:25 +0300

    feat: update

--- travels/travels/doctype/travel_customer/travel_customer.py
+++ travels/travels/doctype/travel_customer/travel_customer.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, Gatura Njenga and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import dateutil
 from frappe.model.document import Document
 
 
 class TravelCustomer(Document):
-	pass
+    pass
+    
+   
