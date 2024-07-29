fa52dfd967f8f74b9ab9665e9c74bc1a96db0b31
GaturaN <gaturanjenga@gmail.com>
2024-07-29 21:07:24 +0300

    refactor: age in travel_customer

--- travels/travels/doctype/travel_customer/travel_customer.js
+++ travels/travels/doctype/travel_customer/travel_customer.js
@@ -9,10 +9,19 @@ frappe.ui.form.on("Travel Customer", {
 });
 
 frappe.ui.form.on("Travel Customer", "date_of_birth", function (frm) {
-    let today = new Date();
-    let birthdate = new Date(frm.doc.date_of_birth);
-    if (today < birthdate) {
-        frappe.msgprint("Invalid Date of Birth");
-        frappe.model.set_value(frm.doctype, frm.docname, "date_of_birth", '');
-    }
+    if (frm.doc.date_of_birth) {
+        let today = new Date();
+        let birthdate = new Date(frm.doc.date_of_birth);
+        if (today < birthdate) {
+            frappe.msgprint("Invalid Date of Birth");
+            frappe.model.set_value(frm.doctype, frm.docname, "date_of_birth", '');
+        }
+        else{
+            let age_str = Math.floor((today - birthdate) / (365.25 * 24 * 60 * 60 * 1000));
+            $(frm.fields_dict['age'].wrapper).html(`${__('AGE')} : ${age_str}`);
+        }
+    } else {
+		$(frm.fields_dict['age'].wrapper).html('');
+	}
+   
 });
--- travels/travels/doctype/travel_customer/travel_customer.py
+++ travels/travels/doctype/travel_customer/travel_customer.py
@@ -1,12 +1,11 @@
 # Copyright (c) 2024, Gatura Njenga and contributors
 # For license information, please see license.txt
 
-import frappe
-import dateutil
+# import frappe
+# import dateutil
 from frappe.model.document import Document
 
 
 class TravelCustomer(Document):
     pass
     
-   
