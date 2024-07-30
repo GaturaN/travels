5509f85ff958effad149a8fe71fcbd716cee9274
GaturaN <gaturanjenga@gmail.com>
2024-07-30 04:38:15 +0300

    feat: full name "travel_customer"

--- travels/travels/doctype/travel_customer/travel_customer.py
+++ travels/travels/doctype/travel_customer/travel_customer.py
@@ -1,11 +1,20 @@
 # Copyright (c) 2024, Gatura Njenga and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 # import dateutil
 from frappe.model.document import Document
 
 
 class TravelCustomer(Document):
-    pass
+
+	def validate(self):
+		first_name = self.first_name.strip().title()
+		last_name = self.last_name.strip().title()
+		self.first_name = first_name
+		self.last_name = last_name
+		self.full_name = f"{first_name} {last_name}"
+  
+		if self.middle_name_optional:
+			self.middle_name_optional= self.middle_name_optional.strip().title()
     
