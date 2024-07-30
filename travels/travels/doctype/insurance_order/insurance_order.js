83ab8e96b3a449a8d0d12f85530519e9a610cdfa
GaturaN <gaturanjenga@gmail.com>
2024-07-30 15:17:20 +0300

    chore: Insurance Order update

--- travels/travels/doctype/insurance_order/insurance_order.js
+++ travels/travels/doctype/insurance_order/insurance_order.js
@@ -6,3 +6,19 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Insurance Order", "from_date", function (frm) {
+  let from_date = new Date(frm.doc.from_date);
+  let to_date = new Date(frm.doc.to_date);
+  let today = new Date();
+
+  // if from_date is less than today
+  if (from_date < today) {
+    frappe.msgprint("Invalid From Date");
+    frappe.model.set_value(frm.doctype, frm.docname, "from_date", "");
+  }
+});
+
+// check to_date
+
+  
