775b57d77447ee113f5f22c8f3e121123fc9ced7
GaturaN <gaturanjenga@gmail.com>
2024-08-05 16:08:10 +0300

    feat: Added Price Doc

---
+++ travels/travels/doctype/price_list/price_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Gatura Njenga and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Price List", {
+// 	refresh(frm) {
+
+// 	},
+// });
