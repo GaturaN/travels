02400e0a4fc97068eac44beae52208e81396ade6
GaturaN <gaturanjenga@gmail.com>
2024-08-09 22:27:09 +0300

    chore: validate from_date

--- travels/travels/doctype/insurance_order/insurance_order.js
+++ travels/travels/doctype/insurance_order/insurance_order.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, Gatura Njenga and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Insurance Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Insurance Order", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+	from_date: function (frm) {
+		let fromDate = new Date(frm.doc.from_date);
+		let today = new Date();
+		if (fromDate < today) {
+			frappe.throw("From date cannot be before today");
+			frm.set_value("from_date", "");
+		}
+
+	}
+});
