4512b0d3c13705b0b16a6963cc2062bbc28e2f3b
GaturaN <gaturanjenga@gmail.com>
2024-08-10 00:13:42 +0300

    chore: message

--- travels/travels/doctype/insurance_order/insurance_order.js
+++ travels/travels/doctype/insurance_order/insurance_order.js
@@ -9,9 +9,27 @@ frappe.ui.form.on("Insurance Order", {
 		let fromDate = new Date(frm.doc.from_date);
 		let today = new Date();
 		if (fromDate < today) {
-			frappe.throw("From date cannot be before today");
+			frappe.msgprint({
+				title:"Warning", 
+				message:"From date cannot be before today", 
+				indicator:"red"});
 			frm.set_value("from_date", "");
+			// frm.refresh();
 		}
 
-	}
+	},
+
+	to_date: function (frm) {
+		if (frm.doc.from_date){
+			let toDate = new Date(frm.doc.to_date);
+			let fromDate = new Date(frm.doc.from_date);
+			if (toDate < fromDate) {
+				frappe.msgprint("To date cannot be before from date");
+				frm.set_value("to_date", "");
+			}
+		} else {
+			frappe.msgprint("Provide from date");
+			// frm.set_value("to_date", "");
+		}
+	},
 });
